5a64f9868b5a9cfbbc63f495510ec362fa99eeed
Stevoni-Dev <54421034+Stevoni-Dev@users.noreply.github.com>
2019-11-01 16:20:39 -0600

    Final Login Page
    Finished the login page. Will add redirects later. Good Logins:
    teacher/teacher
    student/student
    everything else will fail.

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 $( document ).ready(function() {
-    $("#login").submit(function() {
-        console.log("btn hit");
+//------------ LOGIN PAGE METHODS ------------
+    // Password Mng    
+    $("#incorrectPass").hide();
+    
+    $("#hideIncorrectPass").click(function(e){
+        $("#incorrectPass").hide();
     });
+
+    // Login Handler
+    $("#login").submit(function(e) {
+        e.preventDefault();
+        data = $(":input").serializeArray();
+        
+        userName = data[0].value;
+        password = data[1].value;
+        if (userName == "teacher" && password == "teacher") {
+            // Login and redirect to teacher page
+            console.log("Teacher Login");
+        } else if (userName == "student" && password == "student") {
+            console.log("Student Login");
+        } else {
+            $("#incorrectPass").show();
+        }
+        $(".form-control").val("");
+    });
+
 });
 
